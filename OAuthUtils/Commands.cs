262f9a0fcbe3da089062f31b6524ad30797491a0
DerekGn <dg89327@hotmail.com>
2017-05-02 13:20:34 +0100

    Added missing file

---
+++ OAuthUtils/Commands.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.CommandLineUtils;
+
+namespace OAuthUtils
+{
+    internal class Commands : CommandLineApplication
+    {
+        public Commands()
+        {
+            Commands.Add(new DecodeCommand());
+            Commands.Add(new VerifyCommand());
+            HelpOption("-h | -? | --help");
+        }
+    }
+}
